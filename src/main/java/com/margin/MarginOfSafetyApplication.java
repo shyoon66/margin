f2e042455a0f3cab99f36952384a53f3c5c51082
DESKTOP-7IVHS0V\oasis <yoosh2002@hanmail.net>
2018-05-12 13:38:42 +0900

    commit

---
+++ src/main/java/com/margin/MarginOfSafetyApplication.java
@@ -0,0 +1,12 @@
+package com.margin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarginOfSafetyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarginOfSafetyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/margin/MarginOfSafetyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.margin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarginOfSafetyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
